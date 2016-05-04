f515d82787cbd121c6836b77ddc9146a99526fe4
Manuel De Leon <manueldeleon@gmail.com>
2016-05-04 16:57:26 -0600

    compile script

---
+++ compile.bat
@@ -0,0 +1,15 @@
+cd node_modules\vmc2jsx
+call babel --presets es2015 --out-dir dist src
+cd  ..\..
+cd node_modules\viewmodel-react
+call babel --presets es2015 --out-dir dist src
+cd  ..\..
+rem call babel --plugins vmc2jsx --presets es2015,react --out-dir out src
+rem cd ..
+rem cd viewmodel-react-docs
+rd /q/s out
+rd /q/s out_pre
+mkdir out_pre
+xcopy src\*.js out_pre /q/e/h/i
+node replacePreCode.js
+call babel --presets react --out-dir out out_pre
